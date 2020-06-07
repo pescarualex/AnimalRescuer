3383560784d335b2870e9009d37c598ea93c9151
Pescaru Alexandru <pescarualexflorin@gmail.com>
2020-06-07 13:22:06 +0300

    added feeding method and play activity method

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,32 +7,42 @@ public class App
     public static void main( String[] args ) {
 
     Rescuer rescuer = new Rescuer();
-    rescuer.name = "Alex";
-    rescuer.budget = 1000;
+        rescuer.name = "Alex";
+        rescuer.budget = 1000;
 
-    Animal dog = new Animal();
-    dog.name = "Max";
-    dog.age = 5;
-    dog.breed = "German BRAC";
-    dog.favoriteFood = "Dry food";   // like a pedigree
-    dog.favoriteRecreationActivity = "Running after the ball";
-    dog.healthLevel = 2;
-    dog.hungerLevel = 2;
-    dog.moodLevel = 2;
+    Animal animal = new Animal();
+        animal.name = "Max";
+        animal.age = 5;
+        animal.breed = "German BRAC";
+        animal.favoriteFood = "Dry";   // like a pedigree
+        animal.favoriteRecreationActivity = "Running after the ball";
+        animal.healthLevel = 5;
+        animal.hungerLevel = 5;
+        animal.moodLevel = 5;
 
     AnimalFood food = new AnimalFood();
-    food.price = 50;
-    food.name = "Dry food";
-    food.quantity = 15.5;
-    food.expirationDate = LocalDate.of(2020, 8, 8);
+        food.price = 50;
+        food.name = "Dry food";
+        food.quantity = 15.5;
+        food.expirationDate = LocalDate.of(2020, 8, 8);
+    AnimalFood food2 = new AnimalFood();
+        food2.price = 70;
+        food2.name = "Meet";
+        food2.quantity = 20.0;
+        food2.expirationDate = LocalDate.of(2020, 9, 6);
 
     RecreationActivity recreationActivity = new RecreationActivity();
-    recreationActivity.name = "Running after the ball";
+        recreationActivity.name = "Running after the ball";
 
     Veterinarian veterinarian = new Veterinarian();
-    veterinarian.name = "Bill";
-    veterinarian.specialization = "Veterinary medicine";
+        veterinarian.name = "Bill";
+        veterinarian.specialization = "Veterinary medicine";
 
+
+
+
+    rescuer.feeding(animal, food);
+    rescuer.playActivity(animal,recreationActivity);
     }
 }
 
--- src/main/java/org/fasttrackit/Rescuer.java
+++ src/main/java/org/fasttrackit/Rescuer.java
@@ -6,6 +6,12 @@ public class Rescuer{
     double budget;
 
     public void feeding(Animal animal, AnimalFood food){
-        System.out.println();
+        System.out.println(name + " just gave some " + food.name + " food to " + animal.name + ".");
+        animal.hungerLevel--;
+    }
+
+    public  void playActivity(Animal animal, RecreationActivity recreationActivity){
+        System.out.println(name + " is playing " + recreationActivity.name + " with " + animal.name );
+        animal.moodLevel++;
     }
 }
