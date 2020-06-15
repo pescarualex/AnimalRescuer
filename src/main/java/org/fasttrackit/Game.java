744cf2be23f7b126a381412b9d012e68186f12a6
Pescaru Alexandru <pescarualexflorin@gmail.com>
2020-06-15 18:45:44 +0300

    fix error in the Game class

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,7 +1,7 @@
 package org.fasttrackit;
 
 public class Game {
-    Animal animal = new Animal();
+    Animal animal = new Animal("Max", "German BRAC", 5);
     Rescuer rescuer = new Rescuer();
     Veterinarian veterinarian = new Veterinarian();
 
